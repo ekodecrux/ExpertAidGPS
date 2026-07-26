6b7045812b1877bb85fff44032cbf0373aa3e88b
ekodecrux <2.14241291e+08+ekodecrux@users.noreply.github.com>
2026-07-26 16:27:05 +0000

    Change package name to com.expertaid.gpstracking for Google Play Store

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.expertgpstracking.app',
+  appId: 'com.expertaid.gpstracking',
   appName: 'Expert GPS Tracking',
   webDir: 'dist',
   server: {
